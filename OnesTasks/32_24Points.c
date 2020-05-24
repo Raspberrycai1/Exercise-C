c66861e1ff5f010210d4bca8cdfac87f41e45164
Jack Wang <1715936961@qq.com>
2020-05-24 23:59:46 +0800

    copy run success

--- OnesTasks/32_24Points.c
+++ OnesTasks/32_24Points.c
@@ -5,7 +5,8 @@
 #define MAX 8
 
 //每运算一次，将运算符和数字数组变更(前移一位)
-void change(int number[],char symbol[],int i,int k){
+void change(int number[],char symbol[],int i,int k)
+{
     int j;
     for(j=i;j<k-1;j++)
         symbol[j]=symbol[j+1];
@@ -13,12 +14,16 @@ void change(int number[],char symbol[],int i,int k){
         number[j]=number[j+1];
 }
 
-int TwentyFour(int number[],char symbol[]){
+int TwentyFour(int number[],char symbol[])
+{
         int i,j=0,k=3;
         //先算乘除
-        for(i=0;i<k;i++){
-            if(symbol[i]=='x'||symbol[i]=='/'){
-                if(symbol[i]=='x'){
+        for(i=0;i<k;i++)
+	{
+            if(symbol[i]=='x'||symbol[i]=='/')
+	    {
+                if(symbol[i]=='x')
+		{
                     number[i]=number[i]*number[i+1];
                     change(number,symbol,i,k);
                 }
@@ -33,13 +38,17 @@ int TwentyFour(int number[],char symbol[]){
             }
         }
         //后算加减
-        for(i=0;i<k;i++){
-            if(symbol[i]=='+'||symbol[i]=='-'){
-                if(symbol[i]=='+'){
+        for(i=0;i<k;i++)
+	{
+            if(symbol[i]=='+'||symbol[i]=='-')
+	    {
+                if(symbol[i]=='+')
+		{
                     number[i]=number[i]+number[i+1];
                     change(number,symbol,i,k);
                 }
-                else if(symbol[i]=='-'){
+                else if(symbol[i]=='-')
+		{
                     number[i]=number[i]-number[i+1];
                     change(number,symbol,i,k);
                 }
@@ -56,13 +65,15 @@ int main()
     int n,i,j=0,k=0,l;
     char str[MAX];
     scanf("%d",&n);
-    for(l=0;l<n;l++){
+    for(l=0;l<n;l++)
+    {
         scanf("%s",str);
         char symbol[3];
         int number[4];
         j=k=0;
         //从字符串中分割出数字和符号
-        for(i=0;str[i]!='\0';i++){
+        for(i=0;str[i]!='\0';i++)
+	{
             if(isdigit(str[i]))
                 number[j++]=str[i]-'0';
             else
